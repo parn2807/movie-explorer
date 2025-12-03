fdef44df85740a4ff5ad40bed3879241a280ec78
Suphang <Suphang40495@gmail.com>
2025-12-03 15:37:19 +0700

    back to basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route,  Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
@@ -6,14 +6,13 @@ import Search from "./pages/Search";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter basename="/movie-explorer">
      <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/search" element={<Search />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
