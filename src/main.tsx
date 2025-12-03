3d0433f486dce3e417f2d4e5ce49d4470ee4c6e6
Suphang <Suphang40495@gmail.com>
2025-12-03 15:19:31 +0700

    fix HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/movie-explorer">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
