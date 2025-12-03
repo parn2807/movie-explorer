20f545469c82e2a65e5afd817e828a560388fe71
Suphang <Suphang40495@gmail.com>
2025-12-03 15:03:54 +0700

    fix loading api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,  Navigate } from "react-router-dom";
+import { Routes, Route,  Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
@@ -6,7 +6,7 @@ import Search from "./pages/Search";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/movie-explorer">
+    <>
      <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
@@ -14,6 +14,6 @@ export default function App() {
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/search" element={<Search />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
