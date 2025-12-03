9154cf2e20ddeca6d10559af1381c77c58ca57ba
Suphang <Suphang40495@gmail.com>
2025-12-03 14:54:15 +0700

    fix fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,  Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
@@ -6,10 +6,11 @@ import Search from "./pages/Search";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/movie-explorer">
      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/search" element={<Search />} />
       </Routes>
